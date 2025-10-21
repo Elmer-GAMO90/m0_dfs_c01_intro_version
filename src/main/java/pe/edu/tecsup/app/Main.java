ea04de1e274b9d25ae0b6f87a890db553fd3c8de
Elmer <lesnar018@gmail.com>
2025-10-20 20:20:48 -0500

    add comment todo

--- src/main/java/pe/edu/tecsup/app/Main.java
+++ src/main/java/pe/edu/tecsup/app/Main.java
@@ -5,6 +5,8 @@ package pe.edu.tecsup.app;
 public class Main {
     public static void main(String[] args) {
 
+        //TODO
+
         /*
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
